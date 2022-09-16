374a6e8f6d8ab46f504a13189fc2c98dcd7a3e4a
zjuJing <1319422661@qq.com>
2022-09-16 18:01:55 +0800

    finish print pagetable and speed syscall

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void vmprint(pagetable_t pagetable, int depth){
+  if(depth > 2){
+    return;
+  } 
+  if(depth == 0){
+    printf("page table %p\n",pagetable);
+  }
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+      for(int i = 0;i < depth; i++){
+        printf(".. ");
+      }
+      printf("..%d: pte %p pa %p\n",i,pte,child);
+      vmprint((pagetable_t)child,depth+1);
+    }
+  }
+
+}
