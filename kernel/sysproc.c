c1720981087dac53df336ea45d2506deb09cbcc4
zjuJing <1319422661>
2022-10-26 15:24:37 +0800

    finish syscall

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,8 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
+
 
 uint64
 sys_exit(void)
@@ -94,4 +96,36 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+  int mask;
+  if(argint(0, &mask) < 0){
+    return -1;
+  }
+  struct proc *p = myproc();
+  p->trace_mask = mask;
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  struct sysinfo info;
+  struct proc* p = myproc();
+  uint64 addr;
+
+  if(argaddr(0,&addr) < 0){
+    return -1;
+  }
+
+  info.freemem = freemem();
+  info.nproc = nproc();
+  if(copyout(p->pagetable, addr, (char *)&info, sizeof(info)) < 0){
+      return -1;
+  }
+  
+  return 0;
 }
